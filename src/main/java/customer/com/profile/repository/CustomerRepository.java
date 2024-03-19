04f66e1b71ee5621c25a4a623bfea18ba25d4870
Martin Okafor <martin.okafor@diconium.com>
2024-03-19 11:17:07 +0100

    adjust test and add docker file

--- src/main/java/customer/com/profile/repository/CustomerRepository.java
+++ src/main/java/customer/com/profile/repository/CustomerRepository.java
@@ -2,8 +2,11 @@ package customer.com.profile.repository;
 
 import customer.com.profile.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     List<Customer> findByCity(String city);
 
---
+++ src/test/java/customer/com/profile/ProfileApplicationTest.java
@@ -0,0 +1,15 @@
+package customer.com.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProfileApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+
+
+}
