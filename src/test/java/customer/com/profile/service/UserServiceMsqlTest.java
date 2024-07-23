4538c3d66f78a8533b535240a8c66b9cf10733a3
Martin Okafor <martin.okafor@diconium.com>
2024-07-23 09:25:08 +0200

    remove unused import

--- src/test/java/customer/com/profile/service/UserServiceMsqlTest.java
+++ src/test/java/customer/com/profile/service/UserServiceMsqlTest.java
@@ -7,15 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-//@Transactional
 @SpringBootTest
 public class UserServiceMsqlTest {
     @Autowired
