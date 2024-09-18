caf0814fef7e57c4a2e59301e97e5f141e58351a
Martin Okafor <martin.okafor@diconium.com>
2024-09-18 02:07:21 +0200

    add

---
+++ src/main/java/customer/com/profile/logging/Logging.java
@@ -0,0 +1,6 @@
+package customer.com.profile.logging;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class Logging {
+    public Logger logger = LoggerFactory.getLogger(Logging.class);
+}
